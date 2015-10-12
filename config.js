ee174bc144ce7d072bcbc38efdb4522002b734c6
Josh Baker <joshualbaker111@gmail.com>
2015-10-12 11:03:04 -0700

    server finished

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	PORT: process.env.PORT || "3000",
+	// MONGO_URI: process.env.MONGOLAB_URI || "localhost/mongo/hash_it"
+	SESSION_SECRET: process: process.env.SESSION_SECRET || 'OurSuperSecretCookieSecret'
+
+
+
+	EVIRONMENT: require(./env/ + process.env.NODE_ENV + '.js'),
+	TOKEN_SECRET: process: process.env.TOKEN_SECRET || 'xxxxxxxxxxxxxx',
+	INSTAGRAM_SECRET: process: process.env.INSTAGRAM_ID || '932bebc9cffc4be5bcde1602b71a37d6'
+  INSTAGRAM_ACCESS: process: process.env.INSTAGRAM_SECRET || '473020249.932bebc.ac928e49100d436da63fe63a91bfdce0'
+
+}
